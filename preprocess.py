5bc2122b065facb30826f92e3058025685f99243
Jeff Chen <chen.jeff@gmail.com>
2018-10-21 14:22:36 -0400

    preprocess checkin

---
+++ preprocess.py
@@ -0,0 +1,44 @@
+import gzip
+import collections
+import json
+
+RAW_PATH = "data/raw/reviews_Electronics.json.gz"
+PROCESSED_PATH = "data/processed/single_product.json"
+TO_PRINT = 10
+TO_PROCESS = 100000 #-1 means process all
+PICKED_PRODUCT = "B00001WRSJ"
+
+def parse (path):
+  g = gzip.open (path, 'r')
+  for l in g:
+    yield eval (l)
+
+
+ratings = []
+products = collections.defaultdict(list)
+products_review_count = collections.defaultdict(int)
+count = 0
+for review in parse (RAW_PATH):
+  products[review['asin']].append(review)
+  products_review_count[review['asin']] += 1
+  count += 1
+  if count % 100000 == 0:
+    print ("Processed: " + str(count))
+
+  if count == TO_PROCESS and TO_PROCESS != -1:
+    break
+
+p_sorted = sorted(products_review_count.items(), key = lambda item: item[1], 
+                  reverse = True)
+
+print ("MOST REVIEWED PRODUCTS")
+for idx in range(TO_PRINT):
+  key, value = p_sorted [idx]
+  print (key + ": " + str (products_review_count [key]))
+
+
+print ("Saving product " + PICKED_PRODUCT)
+with open(PROCESSED_PATH, 'w') as f:
+  for review in products[PICKED_PRODUCT]:
+    json.dump(review, f, ensure_ascii=False)
+    f.write('\n')
