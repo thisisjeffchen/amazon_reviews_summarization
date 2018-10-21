ba49c2470fc932f5ec4fb84abb8d1714d4b1c8e2
bh2590 <hanozbhathena@gmail.com>
2018-10-21 18:19:40 -0400

    initial commit: db, encoder, k-means on 2 products

---
+++ hb/config.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 20 14:59:36 2018
+
+@author: hanozbhathena
+"""
+
+import argparse
+import ast
+
+parser= argparse.ArgumentParser()
+
+parser.add_argument('--encoder_name', type=str, 
+                    default='nnlm',
+                    help='encoder type: can be nnlm, word2vec, use or elmo')
+
+parser.add_argument('--trainable_embeddings', type=ast.literal_eval, 
+                    default=False,
+                    help='whether tf-hub encoder should be trainable')
+
+parser.add_argument('--min_review_count', type=int, 
+                    default=50,
+                    help='the minimum number of reviews a product must have')
+
+parser.add_argument('--all_review_countdf', type=str, 
+                    default='num_reviews.csv',
+                    help='the name of file containing the review counts per asin for all asins')
+
+parser.add_argument('--filt_review_countdf', type=str, 
+                    default='num_reviews_filt.csv',
+                    help='the name of file containing the review counts per asin for filtered asins')
+
+parser.add_argument('--train_chunksize', type=int, 
+                    default=100,
+                    help='the number of product asins queried at a time')
+
+parser.add_argument('--train_batches', type=int, 
+                    default=2,
+                    help='the number of product groups taken each of chunksize')
+
+parser.add_argument('--test_chunksize', type=int, 
+                    default=100,
+                    help='the number of product asins queried at a time')
+
+parser.add_argument('--test_batches', type=int, 
+                    default=1,
+                    help='the number of product groups taken each of chunksize')
+
+args = parser.parse_args()
---
+++ hb/text_encoders.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 20 15:05:12 2018
+
+@author: hanozbhathena
+"""
+
+import tensorflow as tf
+import tensorflow_hub as hub
+
+
+class BaseHubModel(object):
+    def __init__(self):
+        self.sess= tf.Session()
+        self.sess.run(tf.global_variables_initializer())
+        self.sess.run(tf.tables_initializer())
+    
+    def __del__(self):
+        self.sess.close()
+
+
+class NNLM(BaseHubModel):
+    def __init__(self, path= "https://tfhub.dev/google/nnlm-en-dim128-with-normalization/1",
+                 trainable= False, name= 'nnlm'):
+        self.model= hub.Module(path, trainable= trainable, name= name)
+        super().__init__()
+    
+    def __call__(self, inp):
+        return self.sess.run(self.model(inp))
+
+
+class Word2Vec(BaseHubModel):
+    def __init__(self, path= "https://tfhub.dev/google/nnlm-en-dim128-with-normalization/1",
+                 trainable= False, name= 'word2vec'):
+        self.model= hub.Module(path, trainable= trainable, name= name)
+        super().__init__()
+    
+    def __call__(self, inp):
+        return self.sess.run(self.model(inp))
+
+
+class USE(BaseHubModel):
+    def __init__(self, path= "https://tfhub.dev/google/nnlm-en-dim128-with-normalization/1",
+                 trainable= False, name= 'use'):
+        self.model= hub.Module(path, trainable= trainable, name= name)
+        super().__init__()
+    
+    def __call__(self, inp):
+        return self.sess.run(self.model(inp))
+
+
+class ELMO(BaseHubModel):
+    def __init__(self, path= "https://tfhub.dev/google/nnlm-en-dim128-with-normalization/1",
+                 trainable= False, name= 'elmo'):
+        self.model= hub.Module(path, trainable= trainable, name= name)
+        super().__init__()
+    
+    def __call__(self, inp):
+        raise NotImplementedError("Not implemented call for ELMO")
+
