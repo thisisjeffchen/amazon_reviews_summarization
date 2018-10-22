5ab139daa44e1da16d8fbbeca8494bce9290d3fb
Jeff Chen <chen.jeff@gmail.com>
2018-10-21 20:55:32 -0400

    summaries

--- preprocess.py
+++ preprocess.py
@@ -6,7 +6,7 @@ RAW_PATH = "data/raw/reviews_Electronics.json.gz"
 PROCESSED_PATH = "data/processed/"
 TO_PRINT = 25
 TO_PROCESS = -1 #-1 means process all
-PICKED_PRODUCTS = ["B00001WRSJ", "B006ML9J4O", "B007I5JT4S"]
+PICKED_PRODUCTS = ["B00001WRSJ", "B009AYLDSU", "B007I5JT4S"]
 
 def parse (path):
   g = gzip.open (path, 'r')
--- train.py
+++ train.py
@@ -1,60 +1,80 @@
+import numpy as np
 import argparse
 import json
 import pdb
 import tensorflow as tf
 import tensorflow_hub as hub
+from sklearn.cluster import KMeans
+from nltk.tokenize import sent_tokenize
 
-REVIEWS_PATH = "data/processed/single_product.json"
+
+REVIEWS_PATH = "data/processed/"
+PICKED_PRODUCTS = ["B00001WRSJ", "B009AYLDSU", "B007I5JT4S"]
+RESULTS_PATH = "results/"
 
 def main():
   parser = argparse.ArgumentParser ()
-  parser.add_argument ('--embed', type=str, default="w2v",
+  parser.add_argument ('--embed', type=str, default="nnlm",
                        help="choose amongst different models")
+  parser.add_argument ('--clusters', type=int, default=5,
+                       help="5 clusters for kmeans")
+  args = parser.parse_args ()
 
-  reviews = get_reviews ()
-  all_sentences, review_indicies = segment (reviews)
-  print (all_sentences)
-  all_sentences=all_sentences[0:10]
-  embedded = embed (all_sentences)  
-
+  for product in PICKED_PRODUCTS:
+    reviews = get_reviews (args, product)
+    #all_sentences, review_indicies = segment (args, reviews)
+    #print (all_sentences)
+    #all_sentences=all_sentences[0:10]
   
-  print (len(all_sentences))
-  print (len(embedded))
+    reviewTexts = [review['reviewText'] for review in reviews ]
+    all_sentences = [sent for review in reviewTexts for sent in sent_tokenize(review)]
+  
+    print ("Embedding...")
+    embedded = embed (args, all_sentences)  
+  
+    print ("Running kmeans...")
+    clusters = KMeans(n_clusters = args.clusters, random_state = 0).fit (embedded)  
+    for centroid in range(1, args.clusters + 1):
+      
 
-  print (embedded)
-  #grab a model
+    dist = clusters.transform (embedded)
+    product_reviews_np = np.array (all_sentences)
+    summaries = product_reviews_np[np.argmin (dist, axis = 0)]
+  
+    with open(RESULTS_PATH + "5_summary_" + product + ".json", 'w') as f:
+      json.dump(summaries.tolist(), f, ensure_ascii=False, indent=2)
 
-def get_reviews ():
-  print ("here")
-  with open(REVIEWS_PATH) as f:
+
+def get_reviews (args, product):
+  with open(REVIEWS_PATH + product + ".json") as f:
     data = json.load(f)
   return data
 
-def segment (reviews):
-  all_sentences = []
-  review_indicies = []
-  for idx in range (len (reviews)):
-    review = reviews[idx]
-    text = review ['reviewText']
-    sentences = text.split (". ")
-    for sent in sentences:
-      all_sentences.append (sent)
-      review_indicies.append (idx)
+# def segment (args, reviews):
+#   all_sentences = []
+#   review_indicies = []
+#   for idx in range (len (reviews)):
+#     review = reviews[idx]
+#     text = review ['reviewText']
+#     sentences = text.split (". ")
+#     for sent in sentences:
+#       all_sentences.append (sent)
+#       review_indicies.append (idx)
 
-  return all_sentences, review_indicies
+#   return all_sentences, review_indicies
 
-def embed (all_sentences):
+def embed (args, all_sentences):
   with tf.Graph().as_default():
-  module_url = "https://tfhub.dev/google/nnlm-en-dim128-with-normalization/1"
-  embed = hub.Module(module_url)
-  embeddings = embed(all_sentences)
+    if args.embed == "nnlm":
+      module_url = "https://tfhub.dev/google/nnlm-en-dim128-with-normalization/1"
+      embed = hub.Module(module_url)
+      embeddings = embed(all_sentences)
 
-  with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    sess.run(tf.tables_initializer())
+      with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.tables_initializer())
+        return sess.run(embeddings)
 
-    return sess.run(embeddings)
-    
 def cluster ():
   pass
 
