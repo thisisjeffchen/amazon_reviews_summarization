8a826ca46dde6e34d6d52b9c0a783c30a866c16c
Jeff Chen <chen.jeff@gmail.com>
2019-04-16 12:51:41 -0400

    add benchmark files

---
+++ bench_sentiment.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+CUDA_VISIBLE_DEVICES=0 python sentiment_analysis/sentiment_train.py 
---
+++ bench_sentiment_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for d in 0 1 2 3
+do
+    echo "DEVICE:"
+    echo $d
+    CUDA_VISIBLE_DEVICES=$d time python sentiment_analysis/sentiment_train.py
+done
