6f09622d13b660b32ec9845267d2c11b35761529
Jeff Chen <chen.jeff@gmail.com>
2019-04-17 09:37:54 -0400

    modify benches and move back to 40 epochs

--- bench_sentiment.sh
+++ bench_sentiment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0  time python sentiment_analysis/sentiment_train.py  --epochs 10
+CUDA_VISIBLE_DEVICES=0  time python sentiment_analysis/sentiment_train.py  --epochs 40
